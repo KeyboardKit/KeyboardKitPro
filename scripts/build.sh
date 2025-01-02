aaf1f6684ac5a6961fc03e4ef85330da9cc8d5de
Daniel Saidi <daniel.saidi@gmail.com>
2025-01-02 13:42:54 +0100

    Update package scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,38 +1,65 @@
 #!/bin/bash
 
 # Documentation:
-# This script builds a <TARGET> for all supported platforms.
+# This script builds a <TARGET> for all provided <PLATFORMS>.
+
+# Usage:
+# build.sh <TARGET> [<PLATFORMS> default:iOS macOS tvOS watchOS xrOS]
+# e.g. `bash scripts/build.sh MyTarget iOS macOS`
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
 # Verify that all required arguments are provided
 if [ $# -eq 0 ]; then
-    echo "Error: This script requires exactly one argument"
-    echo "Usage: $0 <TARGET>"
+    echo "Error: This script requires at least one argument"
+    echo "Usage: $0 <TARGET> [<PLATFORMS> default:iOS macOS tvOS watchOS xrOS]"
+    echo "For instance: $0 MyTarget iOS macOS"
     exit 1
 fi
 
-# Create local argument variables
+# Define argument variables
 TARGET=$1
 
-# Array of platforms to build
-PLATFORMS=("iOS" "macOS" "tvOS" "watchOS" "xrOS")
+# Remove TARGET from arguments list
+shift
 
-# Find the build script in the same folder and make is executable
-FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPT="$FOLDER/build_platform.sh"
-chmod +x $SCRIPT
+# Define platforms variable
+if [ $# -eq 0 ]; then
+    set -- iOS macOS tvOS watchOS xrOS
+fi
+PLATFORMS=$@
 
-# Build each platform
-for PLATFORM in "${PLATFORMS[@]}"; do
-    echo "Building for $PLATFORM..."
-    if ! bash $SCRIPT $TARGET $PLATFORM; then
-        echo "Failed to build $PLATFORM"
+# A function that builds $TARGET for a specific platform
+build_platform() {
+
+    # Define a local $PLATFORM variable
+    local PLATFORM=$1
+
+    # Build $TARGET for the $PLATFORM
+    echo "Building $TARGET for $PLATFORM..."
+    if ! xcodebuild -scheme $TARGET -derivedDataPath .build -destination generic/platform=$PLATFORM; then
+        echo "Failed to build $TARGET for $PLATFORM"
+        return 1
+    fi
+
+    # Complete successfully
+    echo "Successfully built $TARGET for $PLATFORM"
+}
+
+# Start script
+echo ""
+echo "Building $TARGET for [$PLATFORMS]..."
+echo ""
+
+# Loop through all platforms and call the build function
+for PLATFORM in $PLATFORMS; do
+    if ! build_platform "$PLATFORM"; then
         exit 1
     fi
-    echo "Successfully built $PLATFORM"
 done
 
-# Success
-echo "All platforms built successfully!"
+# Complete successfully
+echo ""
+echo "Building $TARGET completed successfully!"
+echo ""
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,38 +1,85 @@
 #!/bin/bash
 
 # Documentation:
-# This script tests a <TARGET> for all supported platforms.
+# This script tests a <TARGET> for all provided <PLATFORMS>.
+
+# Usage:
+# test.sh <TARGET> [<PLATFORMS> default:iOS macOS tvOS watchOS xrOS]
+# e.g. `bash scripts/test.sh MyTarget iOS macOS`
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
 # Verify that all required arguments are provided
 if [ $# -eq 0 ]; then
-    echo "Error: This script requires exactly one argument"
-    echo "Usage: $0 <TARGET>"
+    echo "Error: This script requires at least one argument"
+    echo "Usage: $0 <TARGET> [<PLATFORMS> default:iOS macOS tvOS watchOS xrOS]"
+    echo "For instance: $0 MyTarget iOS macOS"
     exit 1
 fi
 
-# Create local argument variables
+# Define argument variables
 TARGET=$1
 
-# Array of platforms to test
-PLATFORMS=("platform=iOS_Simulator,name=iPhone_16")
+# Remove TARGET from arguments list
+shift
 
-# Find the build script in the same folder and make is executable
-FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPT="$FOLDER/test_platform.sh"
-chmod +x $SCRIPT
+# Define platforms variable
+if [ $# -eq 0 ]; then
+    set -- iOS macOS tvOS watchOS xrOS
+fi
+PLATFORMS=$@
 
-# TestBuild each platform
-for PLATFORM in "${PLATFORMS[@]}"; do
+# Start script
+echo ""
+echo "Testing $TARGET for [$PLATFORMS]..."
+echo ""
+
+# A function that tests $TARGET for a specific platform
+test_platform() {
+
+    # Define a local $PLATFORM variable
+    local PLATFORM="${1//_/ }"
+
+    # Define the destination, based on the $PLATFORM
+    case $PLATFORM in
+        "iOS")
+            DESTINATION="platform=iOS Simulator,name=iPhone 16"
+            ;;
+        "macOS")
+            DESTINATION="platform=macOS"
+            ;;
+        "tvOS")
+            DESTINATION="platform=tvOS Simulator,name=Apple TV"
+            ;;
+        "watchOS")
+            DESTINATION="platform=watchOS Simulator,name=Apple Watch"
+            ;;
+        "xrOS")
+            DESTINATION="platform=xrOS Simulator,name=Apple Vision Pro"
+            ;;
+        *)
+            echo "Error: Unsupported platform '$PLATFORM'"
+            exit 1
+            ;;
+    esac
+
+    # Test $TARGET for the $DESTINATION
     echo "Testing $TARGET for $PLATFORM..."
-    if ! bash $SCRIPT $TARGET $PLATFORM; then
-        echo "Failed to test $PLATFORM"
+    xcodebuild test -scheme $TARGET -derivedDataPath .build -destination "$DESTINATION" -enableCodeCoverage YES;
+
+    # Complete successfully
+    echo "Successfully tested $TARGET for $PLATFORM"
+}
+
+# Loop through all platforms and call the test function
+for PLATFORM in $PLATFORMS; do
+    if ! test_platform "$PLATFORM"; then
         exit 1
     fi
-    echo "Successfully tested $PLATFORM"
 done
 
-# Success
-echo "All platforms tested successfully!"
+# Complete successfully
+echo ""
+echo "Testing $TARGET completed successfully!"
+echo ""
