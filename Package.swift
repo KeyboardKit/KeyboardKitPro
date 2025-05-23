3180b8cf6ff71dec684c691ebdeb45185c73feb1
Daniel Saidi <daniel.saidi@gmail.com>
2025-05-23 15:17:35 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KeyboardKitPro",
-            url: "https://github.com/KeyboardKit/KeyboardKit/releases/download/9.5.1/KeyboardKitPro.zip",
-            checksum: "51ffade5ae3922929097ba3e56bdc9497781c4cd5044009713af895232af535e"
+            url: "https://github.com/KeyboardKit/KeyboardKit/releases/download/9.5.2/KeyboardKitPro.zip",
+            checksum: "f233561984af00cd4e8baee4495cc0f59c6424096cb1af5a25fdb6f1587d33a8"
         )
     ]
 )
